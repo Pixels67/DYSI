9f371e9058668a39b84a96b817b117dee4b23959
Pixels <pixels67@proton.me>
2026-01-05 20:12:25 +0300

    Siren model, some SFX, and glitching

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(AudioSource))]
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")] [SerializeField] private float moveSpeed = 10.0f;
     [SerializeField] private bool normalizeInput = true;
     [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private Constraint cameraMovement;
+    [SerializeField] private Constraint cameraWobble;
+    [SerializeField] private float cameraMovementSpeed;
 
     [Header("Rotation")] [SerializeField] private float sensitivity = 0.2f;
     [SerializeField] private Constraint verticalConstraint = new (-90.0f, 70.0f);
@@ -15,8 +19,13 @@ public class PlayerController : MonoBehaviour
 
     private CharacterController _characterController;
     private Camera _camera;
+    private Vector3 _initialCamPos;
     private Vector2 _prevMousePosition;
     private float _verticalAngle;
+    private float _movement = 1.0f;
+    private float _wobble = 0.0f;
+    private float _wobbleTimer = 0.0f;
+    private float _retentionTimer = 0.0f;
 
     private void Awake()
     {
@@ -37,6 +46,10 @@ public class PlayerController : MonoBehaviour
         {
             Debug.LogError("Player Controller: Camera not found!");
         }
+        else
+        {
+            _initialCamPos = _camera.transform.localPosition;
+        }
     }
 
     private void Start()
@@ -56,6 +69,9 @@ public class PlayerController : MonoBehaviour
 
     private void UpdatePosition()
     {
+        _wobbleTimer += Time.deltaTime;
+        _retentionTimer += Time.deltaTime;
+        
         var moveInput = moveAction.action.ReadValue<Vector2>();
         var moveDir = new Vector3(moveInput.x, 0.0f, moveInput.y);
         if (normalizeInput)
@@ -67,6 +83,40 @@ public class PlayerController : MonoBehaviour
         var relativeMoveVec = moveVec.x * transform.right + moveVec.y * transform.up + moveVec.z * transform.forward;
         
         _characterController.Move(relativeMoveVec * Time.deltaTime);
+
+        if (relativeMoveVec.magnitude < 0.1f)
+        {
+            var y = Mathf.Lerp(_camera.transform.localPosition.y, _initialCamPos.y, _retentionTimer);
+            _camera.transform.localPosition = new Vector3(_camera.transform.localPosition.x, y, _camera.transform.localPosition.z);
+            return;
+        }
+
+        _retentionTimer = 0.0f;
+
+        if (_camera.transform.localPosition.y - _initialCamPos.y < cameraMovement.min)
+        {
+            _movement = 1.0f;
+            _camera.transform.localPosition = new Vector3(_camera.transform.localPosition.x, _initialCamPos.y + cameraMovement.min, _camera.transform.localPosition.z);
+            GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.2f);
+            GetComponent<AudioSource>().Play();
+            
+        } else if (_camera.transform.localPosition.y - _initialCamPos.y > cameraMovement.max)
+        {
+            _movement = -1.0f;
+        }
+        
+        _camera.transform.localPosition += _movement * cameraMovementSpeed * Time.deltaTime * Vector3.up;
+        _camera.transform.localPosition += _wobble * cameraMovementSpeed * Time.deltaTime * Vector3.right;
+
+        var camPos = _camera.transform.localPosition;
+        camPos.x = Mathf.Clamp(camPos.x, cameraWobble.min, cameraWobble.max);
+        _camera.transform.localPosition = camPos;
+
+        if (_wobbleTimer >= 0.1f)
+        {
+            _wobbleTimer = 0.0f;
+            _wobble = Random.Range(-0.5f, 0.5f);
+        }
     }
 
     private void UpdateRotation()
